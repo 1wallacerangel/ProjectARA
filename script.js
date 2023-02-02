2385988d17421deec3c2ff1a04305a9325b9c231
Wallace Rangel <wallacerangelagama7@gmail.com>
2023-02-01 23:55:54 -0300

    0.1.0

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ var mark = document.querySelector('.mark');
 document.querySelector('.mark').onclick = () => {
     mark.classList.toggle('active');
 }
+
+var mark = document.querySelector('.mark2');
+
+document.querySelector('.mark').onclick = () => {
+    mark.classList.toggle('active');
+}
